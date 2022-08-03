fc639cff98b385dc347e5cc99b7c8e25aab502bc
Jackson L. Oliveira <jackson.lourenco.oliveira@gmail.com>
2022-08-03 00:08:19 -0300

    Implementação do .js sobre o Campo de Experiências Profissionais

---
+++ ExperienciaProfissional.js
@@ -0,0 +1,95 @@
+const campoDataEntrada1 = document.querySelector(".DataEntrada1");
+const campoDataSaida1 = document.querySelector(".DataSaida1");
+const campoEmpresa1 = document.querySelector(".Empresa1");
+const campoCargo1 = document.querySelector(".Cargo1");
+const campodescCargo1 = document.querySelector(".descCargo1");
+const campoPChaves1 = document.querySelector(".PChaves1");
+const send1ExP = document.querySelector(".EnviarExP1");
+
+send1ExP.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullDataEntrada1 = campoDataEntrada1.value;
+    const fullDataSaida1 = campoDataSaida1.value;
+    const fullEmpresa1 = campoEmpresa1.value;
+    const fullCargo1 = campoCargo1.value;
+    const fulldescCargo1 = campodescCargo1.value;
+    const fullPChaves1 = campoPChaves1.value;
+    document.querySelector("#DataEntrada1").innerHTML = fullDataEntrada1;
+    document.querySelector("#DataSaida1").innerHTML = fullDataSaida1;
+    document.querySelector("#Empresa1").innerHTML = fullEmpresa1;
+    document.querySelector("#Cargo1").innerHTML = fullCargo1;
+    document.querySelector("#descCargo1").innerHTML = fulldescCargo1;
+    document.querySelector("#PChaves1").innerHTML = fullPChaves1;
+});
+
+const campoDataEntrada2 = document.querySelector(".DataEntrada2");
+const campoDataSaida2 = document.querySelector(".DataSaida2");
+const campoEmpresa2 = document.querySelector(".Empresa2");
+const campoCargo2 = document.querySelector(".Cargo2");
+const campodescCargo2 = document.querySelector(".descCargo2");
+const campoPChaves2 = document.querySelector(".PChaves2");
+const send2ExP = document.querySelector(".EnviarExP2");
+
+send2ExP.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullDataEntrada2 = campoDataEntrada2.value;
+    const fullDataSaida2 = campoDataSaida2.value;
+    const fullEmpresa2 = campoEmpresa2.value;
+    const fullCargo2 = campoCargo2.value;
+    const fulldescCargo2 = campodescCargo2.value;
+    const fullPChaves2 = campoPChaves2.value;
+    document.querySelector("#DataEntrada2").innerHTML = fullDataEntrada2;
+    document.querySelector("#DataSaida2").innerHTML = fullDataSaida2;
+    document.querySelector("#Empresa2").innerHTML = fullEmpresa2;
+    document.querySelector("#Cargo2").innerHTML = fullCargo2;
+    document.querySelector("#descCargo2").innerHTML = fulldescCargo2;
+    document.querySelector("#PChaves2").innerHTML = fullPChaves2;
+});
+
+const campoDataEntrada3 = document.querySelector(".DataEntrada3");
+const campoDataSaida3 = document.querySelector(".DataSaida3");
+const campoEmpresa3 = document.querySelector(".Empresa3");
+const campoCargo3 = document.querySelector(".Cargo3");
+const campodescCargo3 = document.querySelector(".descCargo3");
+const campoPChaves3 = document.querySelector(".PChaves3");
+const send3ExP = document.querySelector(".EnviarExP3");
+
+send3ExP.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullDataEntrada3 = campoDataEntrada3.value;
+    const fullDataSaida3 = campoDataSaida3.value;
+    const fullEmpresa3 = campoEmpresa3.value;
+    const fullCargo3 = campoCargo3.value;
+    const fulldescCargo3 = campodescCargo3.value;
+    const fullPChaves3 = campoPChaves3.value;
+    document.querySelector("#DataEntrada3").innerHTML = fullDataEntrada3;
+    document.querySelector("#DataSaida3").innerHTML = fullDataSaida3;
+    document.querySelector("#Empresa3").innerHTML = fullEmpresa3;
+    document.querySelector("#Cargo3").innerHTML = fullCargo3;
+    document.querySelector("#descCargo3").innerHTML = fulldescCargo3;
+    document.querySelector("#PChaves3").innerHTML = fullPChaves3;
+});
+
+const campoDataEntrada4 = document.querySelector(".DataEntrada4");
+const campoDataSaida4 = document.querySelector(".DataSaida4");
+const campoEmpresa4 = document.querySelector(".Empresa4");
+const campoCargo4 = document.querySelector(".Cargo4");
+const campodescCargo4 = document.querySelector(".descCargo4");
+const campoPChaves4 = document.querySelector(".PChaves4");
+const send4ExP = document.querySelector(".EnviarExP4");
+
+send4ExP.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullDataEntrada4 = campoDataEntrada4.value;
+    const fullDataSaida4 = campoDataSaida4.value;
+    const fullEmpresa4 = campoEmpresa4.value;
+    const fullCargo4 = campoCargo4.value;
+    const fulldescCargo4 = campodescCargo4.value;
+    const fullPChaves4 = campoPChaves4.value;
+    document.querySelector("#DataEntrada4").innerHTML = fullDataEntrada4;
+    document.querySelector("#DataSaida4").innerHTML = fullDataSaida4;
+    document.querySelector("#Empresa4").innerHTML = fullEmpresa4;
+    document.querySelector("#Cargo4").innerHTML = fullCargo4;
+    document.querySelector("#descCargo4").innerHTML = fulldescCargo4;
+    document.querySelector("#PChaves4").innerHTML = fullPChaves4;
+});
