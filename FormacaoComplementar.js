f7bb079b6d9fb4b9277a820ed6d223bcb6ef76d3
Jackson L. Oliveira <jackson.lourenco.oliveira@gmail.com>
2022-08-02 22:15:45 -0300

    implementação de funcionalidade do campo Formação Complementar

---
+++ FormacaoComplementar.js
@@ -0,0 +1,167 @@
+const campoAno1FC = document.querySelector(".ano1FC");
+const campoInfos1FC = document.querySelector(".infos1FC");
+const send1FC = document.querySelector(".EnviarFC1");
+
+send1FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno1FC = campoAno1FC.value;
+    const fullInfos1FC = campoInfos1FC.value;
+    document.querySelector("#anoFC1").innerHTML = fullAno1FC;
+    document.querySelector("#infosFC1").innerHTML = fullInfos1FC;
+})
+
+const campoAno2FC = document.querySelector(".ano2FC");
+const campoInfos2FC = document.querySelector(".infos2FC");
+const send2FC = document.querySelector(".EnviarFC2");
+
+send2FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno2FC = campoAno2FC.value;
+    const fullInfos2FC = campoInfos2FC.value;
+    document.querySelector("#anoFC2").innerHTML = fullAno2FC;
+    document.querySelector("#infosFC2").innerHTML = fullInfos2FC;
+})
+
+const campoAno3FC = document.querySelector(".ano3FC");
+const campoInfos3FC = document.querySelector(".infos3FC");
+const send3FC = document.querySelector(".EnviarFC3");
+
+send3FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno3FC = campoAno3FC.value;
+    const fullInfos3FC = campoInfos3FC.value;
+    document.querySelector("#anoFC3").innerHTML = fullAno3FC;
+    document.querySelector("#infosFC3").innerHTML = fullInfos3FC;
+})
+
+const campoAno4FC = document.querySelector(".ano4FC");
+const campoInfos4FC = document.querySelector(".infos4FC");
+const send4FC = document.querySelector(".EnviarFC4");
+
+send4FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno4FC = campoAno4FC.value;
+    const fullInfos4FC = campoInfos4FC.value;
+    document.querySelector("#anoFC4").innerHTML = fullAno4FC;
+    document.querySelector("#infosFC4").innerHTML = fullInfos4FC;
+})
+
+const campoAno5FC = document.querySelector(".ano5FC");
+const campoInfos5FC = document.querySelector(".infos5FC");
+const send5FC = document.querySelector(".EnviarFC5");
+
+send5FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno5FC = campoAno5FC.value;
+    const fullInfos5FC = campoInfos5FC.value;
+    document.querySelector("#anoFC5").innerHTML = fullAno5FC;
+    document.querySelector("#infosFC5").innerHTML = fullInfos5FC;
+})
+
+const campoAno6FC = document.querySelector(".ano6FC");
+const campoInfos6FC = document.querySelector(".infos6FC");
+const send6FC = document.querySelector(".EnviarFC6");
+
+send6FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno6FC = campoAno6FC.value;
+    const fullInfos6FC = campoInfos6FC.value;
+    document.querySelector("#anoFC6").innerHTML = fullAno6FC;
+    document.querySelector("#infosFC6").innerHTML = fullInfos6FC;
+})
+
+const campoAno7FC = document.querySelector(".ano7FC");
+const campoInfos7FC = document.querySelector(".infos7FC");
+const send7FC = document.querySelector(".EnviarFC7");
+
+send7FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno7FC = campoAno7FC.value;
+    const fullInfos7FC = campoInfos7FC.value;
+    document.querySelector("#anoFC7").innerHTML = fullAno7FC;
+    document.querySelector("#infosFC7").innerHTML = fullInfos7FC;
+})
+
+const campoAno8FC = document.querySelector(".ano8FC");
+const campoInfos8FC = document.querySelector(".infos8FC");
+const send8FC = document.querySelector(".EnviarFC8");
+
+send8FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno8FC = campoAno8FC.value;
+    const fullInfos8FC = campoInfos8FC.value;
+    document.querySelector("#anoFC8").innerHTML = fullAno8FC;
+    document.querySelector("#infosFC8").innerHTML = fullInfos8FC;
+})
+
+const campoAno9FC = document.querySelector(".ano9FC");
+const campoInfos9FC = document.querySelector(".infos9FC");
+const send9FC = document.querySelector(".EnviarFC9");
+
+send9FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno9FC = campoAno9FC.value;
+    const fullInfos9FC = campoInfos9FC.value;
+    document.querySelector("#anoFC9").innerHTML = fullAno9FC;
+    document.querySelector("#infosFC9").innerHTML = fullInfos9FC;
+})
+
+const campoAno10FC = document.querySelector(".ano10FC");
+const campoInfos10FC = document.querySelector(".infos10FC");
+const send10FC = document.querySelector(".EnviarFC10");
+
+send10FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno10FC = campoAno10FC.value;
+    const fullInfos10FC = campoInfos10FC.value;
+    document.querySelector("#anoFC10").innerHTML = fullAno10FC;
+    document.querySelector("#infosFC10").innerHTML = fullInfos10FC;
+})
+
+const campoAno11FC = document.querySelector(".ano11FC");
+const campoInfos11FC = document.querySelector(".infos11FC");
+const send11FC = document.querySelector(".EnviarFC11");
+
+send11FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno11FC = campoAno11FC.value;
+    const fullInfos11FC = campoInfos11FC.value;
+    document.querySelector("#anoFC11").innerHTML = fullAno11FC;
+    document.querySelector("#infosFC11").innerHTML = fullInfos11FC;
+})
+
+const campoAno12FC = document.querySelector(".ano12FC");
+const campoInfos12FC = document.querySelector(".infos12FC");
+const send12FC = document.querySelector(".EnviarFC12");
+
+send12FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno12FC = campoAno12FC.value;
+    const fullInfos12FC = campoInfos12FC.value;
+    document.querySelector("#anoFC12").innerHTML = fullAno12FC;
+    document.querySelector("#infosFC12").innerHTML = fullInfos12FC;
+})
+
+const campoAno13FC = document.querySelector(".ano13FC");
+const campoInfos13FC = document.querySelector(".infos13FC");
+const send13FC = document.querySelector(".EnviarFC13");
+
+send13FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno13FC = campoAno13FC.value;
+    const fullInfos13FC = campoInfos13FC.value;
+    document.querySelector("#anoFC13").innerHTML = fullAno13FC;
+    document.querySelector("#infosFC13").innerHTML = fullInfos13FC;
+})
+
+const campoAno14FC = document.querySelector(".ano14FC");
+const campoInfos14FC = document.querySelector(".infos14FC");
+const send14FC = document.querySelector(".EnviarFC14");
+
+send14FC.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const fullAno14FC = campoAno14FC.value;
+    const fullInfos14FC = campoInfos14FC.value;
+    document.querySelector("#anoFC14").innerHTML = fullAno14FC;
+    document.querySelector("#infosFC14").innerHTML = fullInfos14FC;
+})
